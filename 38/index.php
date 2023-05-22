b5d215967ef215ec01a90713066e8f01a938c38f
Андрей Шинкарев <shinkarev89@gmail.com>
2023-05-22 16:25:25 +0300

    Add 39 lesson

--- 38/index.php
+++ 38/index.php
@@ -2,7 +2,7 @@
 
 error_reporting(-1);
 
-//setcookie('test', 'TEST1', path: '/');
+setcookie('test', 'TEST1', path: '/');
 //setcookie('test2', 'TEST2', ['path' => '/']);
 //setcookie('test3', 'TEST3', ['path' => '/', 'expires' => time()+10]);
 //setcookie('test4', 'TEST4', path: '/', expires_or_options: time()+3600);
---
+++ 39/funcs.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Return sum of $a and $b
+ * @param int $a
+ * @param int $b
+ * @return int Sum of $a and $b
+ */
+function get_sum(int $a, int $b): int
+{
+    return $a + $b;
+}
+
+/**
+ * Return multiplication of $a and $b
+ * @param int $a
+ * @param int $b
+ * @return int Multiplication of $a and $b
+ */
+function multiplication(int $a, int $b): int
+{
+    return $a * $b;
+}
